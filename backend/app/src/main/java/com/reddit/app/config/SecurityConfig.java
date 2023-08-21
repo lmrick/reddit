cfac4f80bc4d3bad45bb0d73af10cecf907f7903
Rick <raphaallemos@gmail.com>
2023-08-21 12:17:10 -0300

    Spring security defined and auth controller added

--- backend/app/src/main/java/com/reddit/app/config/SecurityConfig.java
+++ backend/app/src/main/java/com/reddit/app/config/SecurityConfig.java
@@ -1,5 +1,20 @@
 package com.reddit.app.config;
 
-public class SecurityConfig {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated());
+    }
 
 }
---
+++ backend/app/src/main/java/com/reddit/app/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.reddit.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+
+
+}
